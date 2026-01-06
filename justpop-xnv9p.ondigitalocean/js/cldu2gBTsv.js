2c278c7a21e1f4b07e88b04927145e4925a9f42c
wadzinski1303shannonpaige <wadzinski1303shannonpaige@gmail.com>
2026-01-06 11:35:52 -0800

    Add files via upload

---
+++ justpop-xnv9p.ondigitalocean/js/cldu2gBTsv.js
@@ -0,0 +1,14 @@
+
+        function dadasdwqerewqaasd(name)
+        {
+            name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+            var regexS = "[\\?&]"+name+"=([^&#]*)";
+            var regex = new RegExp( regexS );
+            var results = regex.exec( window.location.href );
+            if( results == null )
+                return "";
+            else
+                return results[1];
+        }
+        var bcda = '1-855-213-6258';
+  
